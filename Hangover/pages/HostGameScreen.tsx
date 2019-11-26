dc83a255c5b3103f3b22d25f21c069d751ea71c3
Sam McPhail <sam.mcphail9@gmail.com>
2019-11-26 15:06:25 -0500

    Add HostGameScreen

---
+++ Hangover/pages/HostGameScreen.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    FlatList,
+    AsyncStorage,
+} from 'react-native';
+import {ACCENT_GRAY, PRIMARY_DARK,  DEBUG, PRIMARY_LIGHT, SECONDARY, FONT, BASE, serverAddress} from '../styles/common';
+import {heightPercentageToDP as hp, widthPercentageToDP as wp, heightPercentageToDP} from "react-native-responsive-screen";
+import * as Font from 'expo-font'
+import styles from "../styles/hostgamescreen.js";
+import axios from 'axios';
+
+interface Props {
+    navigation: any
+}
+
+export default class HomeScreen extends React.Component<Props> {
+    state = {
+        game: null,
+        quiz: null,
+        loading: true,
+    };
+
+    componentWillMount(){
+        AsyncStorage.getItem("game_name")
+        .then((name) => axios.get(serverAddress + "/game/" + name))
+        .then((res) => this.setState({game: res.data}))
+        .then(() => axios.get(serverAddress + "/quizzes/" + this.state.game.quiz))
+        .then((res) => this.setState({quiz: res.data, loading: false}))
+        .then(() => axios.post(serverAddress + "/game/" + this.state.game.game_name,
+        {user_id: this.state.game.host,
+        player_name: "Host Name"}))
+        .then(() => this.render());
+    }
+
+    update(){
+        let that = this;
+        setTimeout(function () {
+            that.refreshGame();
+          }, 5000);
+    }
+
+    async refreshGame(){
+        axios.get(serverAddress + "/game/" + this.state.game.game_name)
+        .then((res) => this.setState({game: res.data}));
+    }
+
+    renderPlayers(){
+        let toRender = [];
+        for(const element of this.state.game.players)
+            toRender.push(<PlayerBubble uuid={element}></PlayerBubble>);
+        return toRender;
+    }
+
+    render() {
+
+        if(this.state.loading){
+            return (
+                <View style={styles.background}>
+                    <View style={styles.loadingTitleContainer}>
+                        <Text style={styles.titleText}>Loading....</Text>
+                    </View>
+                </View>
+            );
+        }
+
+        this.update();
+
+        return (
+            
+            <View style={styles.background}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.titleText}>{this.state.game.game_name}</Text>
+                    <Text style={styles.headerText}>PLAYERS JOINED</Text>
+                </View>
+                <View style={styles.playerBubblesContainer}>
+                    {this.renderPlayers()}
+                </View>
+                <TouchableOpacity style={styles.bigBtnContainer} onPress={() => {this.props.navigation.navigate("")}}>
+                    <Text style={styles.bigBText}>PLAY</Text>
+                </TouchableOpacity>
+            </View>
+
+        );
+
+    }
+}
+
+class PlayerBubble extends React.Component{
+
+    state={
+        uuid: "",
+        game_name: "",
+    };
+
+    constructor(props){
+        super(props);
+        this.setState({uuid: props.uuid});
+        this.getPlayerName(props.uuid);
+        
+    }
+
+    componentWillMount(){
+        
+    }
+
+    getPlayerName(uuid){
+        axios.get(serverAddress + "/api/players/" + uuid)
+        .then((res) => res.data)
+        .then((player) => player.player_name)
+        .then((name) => this.setState({game_name: name}))
+        .then(() => this.render())
+        .catch((err) => console.debug(err));
+    }
+
+    render(){
+        return(
+            <View style={styles.playerBubbleContainer}>
+                <Image source={require("../assets/icon.png")} style={styles.playerIcon}></Image>
+                <Text style={styles.playerBubbleName}>{this.state.game_name}</Text>
+            </View>
+        )
+    }
+}
---
+++ Hangover/styles/hostgamescreen.js
@@ -0,0 +1,80 @@
+import {StyleSheet} from "react-native";
+import {heightPercentageToDP as hp, widthPercentageToDP as wp} from "react-native-responsive-screen";
+import {ACCENT_GRAY, FONT, PRIMARY_DARK, PRIMARY_LIGHT, SECONDARY, BASE} from "./common";
+
+export default styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: BASE,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    titleContainer: {
+        position: 'absolute',
+        top: hp(10),
+        width: wp(90),
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    loadingTitleContainer: {
+        top: hp(10),
+        width: wp(90),
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    titleText: {
+        fontFamily: FONT,
+        color: PRIMARY_DARK,
+        fontSize: hp(6),
+        textAlign: 'center'
+    },
+    headerText: {
+        marginTop: hp(7),
+        fontFamily: FONT,
+        color: PRIMARY_DARK,
+        fontSize: hp(3.5),
+        textAlign: 'center'
+    },
+    playerBubbleContainer: {
+        top: hp(5),
+        width: wp(100),
+        height: hp(),
+        backgroundColor: BASE,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    playerBubbleContainer: {
+        width: wp(60),
+        height: hp(9),
+        flexDirection: 'row',
+        backgroundColor: "#fff",
+        borderRadius: hp(2),
+        justifyContent: 'space-around',
+    },
+    playerBubbleName: {
+        fontFamily: FONT,
+        fontSize: hp(3.5),
+        color: PRIMARY_DARK,
+        alignSelf: 'center',
+    },
+    playerIcon: {
+        alignSelf: 'center',
+        width: hp(7),
+        height: hp(7),
+    },
+    bigBtnContainer: {
+        position: 'absolute',
+        bottom: 0,
+        width: wp(100),
+        height: hp(12),
+        backgroundColor: PRIMARY_DARK,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    bigBText: {
+        fontFamily: FONT,
+        color: BASE,
+        fontSize: hp(3.5),
+        textAlign: 'center'
+    },
+});
